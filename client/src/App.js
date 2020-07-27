28372ac61408993eeaf9492aae1301da20936cef
sab30-webdev <sabarinath3072001@gmail.com>
2020-07-27 19:53:31 +0530

    9-4

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import Login from "./components/auth/Login";
 import Register from "./components/auth/Register";
 import Alert from "./components/layout/Alert";
 import Dashboard from "./components/dashboard/Dashboard";
+import CreateProfile from "./components/profile-forms/CreateProfile";
 import PrivateRoute from "./components/routing/PrivateRoute";
 
 import "./App.css";
@@ -35,6 +36,11 @@ const App = () => {
           <Route exact path="/login" component={Login} />
           <Route exact path="/register" component={Register} />
           <PrivateRoute exact path="/dashboard" component={Dashboard} />
+          <PrivateRoute
+            exact
+            path="/create-profile"
+            component={CreateProfile}
+          />
         </Switch>
         <Footer />
       </Router>
